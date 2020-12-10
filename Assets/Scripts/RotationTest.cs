7edd616b160cc020b6a3f56a72d60950a8dfdaf0
Isaac Jasper <Isaacjasper42@gmail.com>
2020-12-10 15:09:21 -0800

    test

---
+++ Assets/Scripts/RotationTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationTest : MonoBehaviour
+{
+    public float x = 0;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            x += 0.1f;
+            transform.rotation = new Quaternion(0, 0, 0, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/cubes.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+
+public class cubes : MonoBehaviour
+{
+    public List<GameObject> cubeLine;
+    public int size;
+    public bool rotate;
+    public int rotType;
+    public Vector3 thisSize;
+    public bool rotatetrue;
+
+    void Start() {
+        size = GameObject.FindGameObjectWithTag("CubeCreator").GetComponent<createCube>().size;
+        size = (size * size);
+        cubeLine = new List<GameObject>();
+        thisSize = GetComponent<BoxCollider>().size;
+    }
+    void Update() //testing thing
+    {
+        if (rotatetrue)
+        {
+            rotatetrue = false;
+            Rotate();
+        }
+    }
+    void OnTriggerEnter(Collider col) {
+        if (col.CompareTag("cube")) {
+            cubeLine.Add(col.gameObject);
+        }
+    }
+    void OnTriggerExit(Collider col) {
+        if (col.CompareTag("cube")) {
+            cubeLine.Remove(col.gameObject);
+        }
+    }
+    public void Rotate()
+    {
+        if (thisSize.x == thisSize.y)
+        {
+            rotType = Random.Range(0, 2);
+        }
+        if (thisSize.y == thisSize.z)
+        {
+            rotType = Random.Range(2, 4);
+        }
+        if (thisSize.x == thisSize.z)
+        {
+            rotType = Random.Range(4, 6);
+        }
+        for (int i = 0; i < size; i++)
+        {
+            cubeLine[i].GetComponent<pivots>().rotationDirection(this.transform.position, rotType);
+            cubeLine[i].GetComponent<pivots>().Rotate();
+        }
+    } 
+}
