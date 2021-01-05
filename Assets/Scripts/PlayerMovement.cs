c1624599489aef2c73286fe6d87ec4a0de99c658
Isaac Jasper <Isaacjasper42@gmail.com>
2021-01-05 11:44:58 -0800

    got basic movment and jumping working, havnt messed with it much though, next is the guns

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,11 +4,41 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public CharacterController controller;
+
+    public float speed = 10f;
+    public float gravity = -9.81f;
+    public float groundDistance = 0.4f;
+    public float jumpHeight = 2f;
+
+    public LayerMask groundMask;
+    public Transform groundCheck;
+
+    Vector3 velocity;
+    bool isGrounded;
     void Update()
     {
-        if (Input.GetKeyDown("Vertical"))
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
         {
-            //move forwards
+            velocity.y = -2f;
         }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y += Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }
